799547cca59d67cac5becb151402c5ebd5e744e4
Muhammad Nehal <78788867+cop3043@users.noreply.github.com>
2024-02-29 19:36:52 +0500

    JavaScript-Chapter-01

---
+++ app.js
@@ -0,0 +1,6 @@
+alert("welcome");
+alert("Error! Please enter a valid password");
+alert("Welcome to JS Land.. \n Happy Coding!");
+alert("Welcome to Js Land..."); alert("Happy coding \n Prevent this page from creating additional dialogs");
+document.write ("Hello... I can run JS through my web browser's console")
+
